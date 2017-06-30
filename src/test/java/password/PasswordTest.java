d5787c0b1bf57db2028cad67262326e82cec1a55
snaplucas <lucas.chapola@gmail.com>
2017-06-29 21:35:43 -0300

    +Unit tests

--- src/test/java/password/PasswordTest.java
+++ src/test/java/password/PasswordTest.java
@@ -1,43 +1,69 @@
 package password;
 
+import application.dto.PasswordDto;
+import application.enumeradores.Complexidade;
+import application.interfaces.IPasswordAppService;
+import domain.model.entities.Password;
+import domain.model.interfaces.IPasswordService;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import port.adapter.AppStart;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {AppStart.class})
 public class PasswordTest {
 
-    @Test
-    public void removerMedio() {
-        List<Character> list = Arrays.asList('a', 'b', '1', '1', 'a', 'b');
-        List<Character> characterList = list.stream().skip(1).collect(Collectors.toList());
-        characterList.remove(characterList.size() - 1);
+    @Autowired
+    private IPasswordService passwordService;
 
-        System.out.println(characterList);
+    @Autowired
+    private IPasswordAppService passwordAppService;
+
+
+    @Test
+    public void pontuacaoTest_1() {
+        Password password = new Password("aasdsq1231ascas");
+        double pontuacao = passwordService.calcularPontuacaoTotal(password);
+        Assert.assertEquals(63.0, pontuacao, 0.0);
     }
 
     @Test
-    public void minusculasConsecutivas() {
-        List<Character> list = Arrays.asList('a', 'b', 'c', '1', '1', 'a', 'b', 'd', 'E');
-
-        int ocorrencias = 0;
-        int minusculas = -1;
-
-        for (Character c : list) {
-            if (Character.isLowerCase(c)) {
-                minusculas++;
-            } else {
-                minusculas = -1;
-            }
-
-            if (minusculas > 0) {
-                ocorrencias++;
-            }
-        }
-        System.out.println(ocorrencias);
+    public void pontuacaoTest_2() {
+        Password password = new Password("23421qw$");
+        double pontuacao = passwordService.calcularPontuacaoTotal(password);
+        Assert.assertEquals(72.0, pontuacao, 0.0);
     }
 
     @Test
-    public void compl
+    public void pontuacaoTest_3() {
+        Password password = new Password("123qw");
+        double pontuacao = passwordService.calcularPontuacaoTotal(password);
+        Assert.assertEquals(33, pontuacao, 0.0);
+    }
+
+    @Test
+    public void complexidadeTest_1() {
+        PasswordDto passwordDto = passwordAppService.avaliarPassword("aaa");
+        Assert.assertEquals(passwordDto.getPontuacao(), 0.0, 0.0);
+        Assert.assertEquals(passwordDto.getComplexidade(), Complexidade.MUITO_FRACA);
+    }
+
+    @Test
+    public void complexidadeTest_2() {
+        PasswordDto passwordDto = passwordAppService.avaliarPassword("¨GJ(*");
+        Assert.assertEquals(passwordDto.getPontuacao(), 44.0, 0.0);
+        Assert.assertEquals(passwordDto.getComplexidade(), Complexidade.BOA);
+    }
+
+    @Test
+    public void complexidadeTest_3() {
+        PasswordDto passwordDto = passwordAppService.avaliarPassword("a@ne0124nq34Q");
+        Assert.assertEquals(passwordDto.getPontuacao(), 100.0, 0.0);
+        Assert.assertEquals(passwordDto.getComplexidade(), Complexidade.MUITO_FORTE);
+    }
 }
